7872804b7515f396e0d2d4fc3a6e0b165f4a423c
KDOTGIS <kyleg@ksdot.org>
2013-08-26 16:13:13 -0500

    disconnect

--- WORKFLOW/Administration/Disconnect_Harshly.py
+++ WORKFLOW/Administration/Disconnect_Harshly.py
@@ -6,15 +6,9 @@ Created on Mar 12, 2013
 import arcpy
 import ListUsersProd
 ListUsersProd.main()
-
 admin_workspace = r"Database Connections\SDEPROD_SDE.sde"
 #admin_workspace = r"Database Connections\SDEDEV_SDE.sde"
-
 arcpy.env.workspace = admin_workspace
-
 users = arcpy.ListUsers(admin_workspace) #
-
 arcpy.DisconnectUser(admin_workspace, "All")
-        
 arcpy.AcceptConnections(admin_workspace, True)
-
