a600f6ca062484cede0a8147b206e9751d48efc8
KDOTGIS <kyleg@ksdot.org>
2014-01-14 10:04:29 -0600

    COMMISSION OF DATA ADMINISTRATIOIN SCRIPTS FOR ARCGIS 10.2

--- WORKFLOW/Administration/Disconnect_Harshly.py
+++ WORKFLOW/Administration/Disconnect_Harshly.py
@@ -5,7 +5,8 @@ Created on Mar 12, 2013
 '''
 import arcpy
 #admin_workspace = r"Database Connections\SQL61_AVIATION_DB.sde"
-admin_workspace = r"Database Connections\SDEDev-sde.sde"
+#admin_workspace = r"Database Connections\SDEDev-sde.sde"
+admin_workspace = r"Database Connections\SDEPROD_sde.sde"
 arcpy.env.workspace = admin_workspace
 users = arcpy.ListUsers(admin_workspace) #
 print users
