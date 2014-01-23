d73fc6e818a3cad1989e6c4121778b8720aece7e
KDOTGIS <kyleg@ksdot.org>
2014-01-23 13:42:21 -0600

    test

--- WORKFLOW/Administration/Database_migration.py
+++ WORKFLOW/Administration/Database_migration.py
@@ -4,7 +4,7 @@ Created on Jan 22, 2014
 @author: kyleg
 '''
 import arcpy
-import cx_Oracle
+
 outws = r'\\Database Connections\SDEDEV_SHARED.sde\SHARED.'
 mxd = arcpy.mapping.MapDocument(r"\\gisdata\arcgis\GISdata\MXD\GISPROD2SDEDEV_GIS.mxd")
 for df in arcpy.mapping.ListDataFrames(mxd, "TESTPROJ") :
